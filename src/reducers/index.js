a283793f99dced5238284d5aa4a5d872a5fc1dfa
Dana Truong <tramtramphotos@gmail.com>
2023-02-14 18:08:32 -0600

    Add loadingReducer to rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,12 +2,14 @@ import loggedReducer from "./isLogged";
 import { combineReducers } from "redux";
 import { trailsReducer } from "./trails";
 import { countyReducer } from "./counties";
+import loadingReducer from "./isLoading"
 import assignUsers from "./userAssignment";
 import setIndividualUser from "./setIndividualUser";
 import setIndividualTrail from "./setIndividualTrail";
 
 const rootReducer = combineReducers({
   isLogged: loggedReducer,
+  isLoading: loadingReducer,
   trails: trailsReducer,
   counties: countyReducer,
   users: assignUsers,
