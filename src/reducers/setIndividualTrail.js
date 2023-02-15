961aa5fa24016c88cdb43abd9a0d8345c01d8c91
Dana Truong <tramtramphotos@gmail.com>
2023-02-14 18:07:55 -0600

    Update setIndividualTrail reducer

--- src/reducers/setIndividualTrail.js
+++ src/reducers/setIndividualTrail.js
@@ -1,4 +1,4 @@
-const setIndividualTrail = (state = {}, action) => {
+const setIndividualTrail = (state = {data:{attributes:{name:'no-name'}}}, action) => {
     switch(action.type) {
         case 'SET_TRAIL':
             return action.payload
